af137015e6372ad568d95fbdd03cdadeb7dc355a
nasircste <nasircste15@gmail.com>
2025-09-12 03:47:27 +0600

    Deploy Server side validation,Custom validation,Asp validation summary, Client side validation. And also add edit and delete Buttons. Perform CRUD operation . Also adding TempData + Partial View +Toastr Notification

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,10 +7,12 @@ namespace BulkyWeb.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Name is Required")]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
